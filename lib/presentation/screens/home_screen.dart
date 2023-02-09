8a0b961e990c1f7d9d27ca296db1412cd95c9514
phuochungus <21520252@gm.uit.edu.vn>
2023-02-09 10:53:23 +0700

    finish add BLoC architecture to app

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,88 +1,130 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/repositories/news_repository.dart';
 import "package:flutter/services.dart";
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:news_app/business_login/blocs/home_screen_bloc.dart';
 import 'package:news_app/data/models/news.dart';
+import 'package:news_app/data/repositories/news_repository.dart';
 import 'package:news_app/presentation/widgets/news_list_view.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   List<News> newsGroup = List.empty();
-  @override
-  State<StatefulWidget> createState() {
-    return HomeScreenState();
-  }
-}
-
-class HomeScreenState extends State<HomeScreen> {
-  List<News> newsGroup = List.empty();
-
   final ScrollController _controller = ScrollController();
-  void _scrollUp() {
-    _controller.animateTo(_controller.position.minScrollExtent,
-        duration: const Duration(milliseconds: 500),
-        curve: Curves.fastOutSlowIn);
-  }
 
-  HomeScreenState() {
-    NewsRepository().getAll().then((value) {
-      setState(() {
-        newsGroup = value;
-      });
-    });
-  }
+  SnackBar snackBar = const SnackBar(content: Text('data not load'));
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(
       SystemUiMode.leanBack,
       overlays: [SystemUiOverlay.bottom],
     );
-    return Scaffold(
-      drawer: NavigationDrawer(),
-      appBar: AppBar(
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
-                colors: <Color>[Color(0xff1D1A61), Color(0xff18DAB8)]),
-          ),
-        ),
-        toolbarHeight: 90,
-        title: const Align(
-          alignment: Alignment.center,
-          child: Text(
-            'My News',
-            style: TextStyle(fontSize: 23),
-          ),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _scrollUp(),
-        backgroundColor: const Color(0xb21D1A61),
-        child: Image.asset('assets/images/double_arrow_up.png'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(15, 15, 0, 0),
-            child: Text(
-              'News',
-              style: GoogleFonts.openSans(
-                color: const Color(0xff1D1A61),
-                fontSize: 20,
+    return RepositoryProvider(
+      create: (context) => NewsRepository(),
+      child: BlocProvider(
+        create: (context) =>
+            HomeScreenBloc(RepositoryProvider.of<NewsRepository>(context))
+              ..add(LoadNews()),
+        child: Scaffold(
+          drawer: NavigationDrawer(),
+          appBar: AppBar(
+            flexibleSpace: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: <Color>[Color(0xff1D1A61), Color(0xff18DAB8)]),
+              ),
+            ),
+            toolbarHeight: 90,
+            title: const Align(
+              alignment: Alignment.center,
+              child: Text(
+                'My News',
+                style: TextStyle(fontSize: 23),
               ),
             ),
           ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
-              child: NewsListView(_controller, newsGroup),
-            ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+          floatingActionButton: CustomFAB(_controller),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15, 15, 0, 0),
+                child: Text(
+                  'News',
+                  style: GoogleFonts.openSans(
+                    color: const Color(0xff1D1A61),
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              BlocBuilder<HomeScreenBloc, HomeScreenState>(
+                builder: (context, state) {
+                  if (state is LoadingNewsState) {
+                    return const Align(
+                      child: Text('Loading'),
+                      alignment: Alignment.center,
+                    );
+                  }
+
+                  if (state is LoadedNewsState) {
+                    return Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
+                        child: NewsListView(_controller, state.payload),
+                      ),
+                    );
+                  }
+                  print(state);
+                  return Text('No State');
+                },
+              )
+            ],
           ),
-        ],
+        ),
       ),
+    );
+  }
+}
+
+class CustomFAB extends StatelessWidget {
+  final ScrollController _scrollController;
+  const CustomFAB(this._scrollController);
+
+  void _scrollUp() {
+    _scrollController.animateTo(_scrollController.position.minScrollExtent,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.fastOutSlowIn);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      direction: Axis.vertical,
+      children: [
+        Container(
+          margin: EdgeInsets.all(5),
+          child: FloatingActionButton(
+              onPressed: () {
+                print('pressed');
+                BlocProvider.of<HomeScreenBloc>(context).add(LoadNews());
+              },
+              backgroundColor: const Color(0xb21D1A61),
+              child: Icon(Icons.replay)),
+        ),
+        Container(
+          margin: EdgeInsets.all(5),
+          child: FloatingActionButton(
+            onPressed: () {
+              print('pressed');
+              _scrollUp();
+            },
+            backgroundColor: const Color(0xb21D1A61),
+            child: Image.asset('assets/images/double_arrow_up.png'),
+          ),
+        ),
+      ],
     );
   }
 }
